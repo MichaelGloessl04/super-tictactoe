6ca3b2e63085c38695be99ad8ef15e410239c386
MichaelGloessl04 <gloess190117@gmail.com>
2023-12-05 16:59:43 +0100

    finish

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import pygame
 import time
+import os
 import pandas as pd
 
-heart = """
+grid = """
 --#--#--
 --#--#--
 ########
@@ -31,93 +32,149 @@ cell_size = 40
 
 
 def main():
-    ye = True
-    while ye:
-        game = 0
-        turn = True
-        played = []
+    main_loop = True
+    game = get_game()
+    while main_loop:
+        endscreen = True
+        game_running = True
+
+        rounds = 0  # rounds since start of the game
+        tic_or_toe = True  # whose turn is it
+        played_moves = []  # which fields are occupied
+
         protocol = {
             'game': game,
             'move': [],
-            'time': []
+            'time': [],
+            'winner': 0
         }
+
         tic_map = [[0, 0, 0],
                    [0, 0, 0],
                    [0, 0, 0]]
-        map = new_map(map_size, map_size)
-        map = flip(map[:], import_art(heart))
+
+        map = new_map(map_size, map_size)  # create new map
+        map = flip(map[:], import_art(grid))  # flip ascii art as grid
+
         pygame.init()
         pygame.display.set_caption("TicTacToe")
         screen = pygame.display.set_mode((map_size * cell_size,
                                           map_size * cell_size))
-        running = True
-        while running:
+
+        # start game
+        while game_running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    endscreen = False
+                    game_running = False
+                    main_loop = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    chosen = get_field(pygame.mouse.get_pos())
-                    if played.count(chosen) <= 0:
-                        map = place_mark(map[:],
-                                         chosen,
-                                         turn)
-                        turn = update_turn(turn)
-                        played.append(chosen)
-                        protocol['move'].append(chosen)
-                        protocol['time'].append(time.time())
+                    chosen_field = get_field(pygame.mouse.get_pos())  # get which field was clicked on
+
+                    if played_moves.count(chosen_field) <= 0:  # check if field is occupied
+                        map = place_mark(map[:],  # place ascii art on map
+                                         chosen_field,
+                                         tic_or_toe)
+
+                        # update needed variables
+                        tic_or_toe = update_turn(tic_or_toe)
+                        played_moves.append(chosen_field)
+                        rounds += 1
                         tic_map = update_tic_map(tic_map[:],
-                                                 chosen,
-                                                 turn)
+                                                 chosen_field,
+                                                 tic_or_toe)
+
+                        # update protocol
+                        protocol['move'].append(chosen_field)
+                        protocol['time'].append(int(time.time()))
+
+                        # check if a player has won
                         if win_cond(tic_map) == 1:
-                            print(1)
-                            running = False
+                            protocol['winner'] = 1
+                            game_running = False
                         elif win_cond(tic_map) == 2:
-                            print(2)
-                            running = False
-            for i in range(map_size):
+                            protocol['winner'] = 2
+                            game_running = False
+                        elif rounds >= 9:  # tie
+                            game_running = False
+
+            for i in range(map_size): 
                 for k in range(map_size):
                     color = white if map[i][k] else black
                     pygame.draw.rect(screen, color, (k * cell_size,
                                                      i * cell_size,
                                                      cell_size, cell_size))
             pygame.display.flip()
-        export(protocol)
-        game += 1
+        
+        game += 1  # update game id
+        export(protocol)  # append to csv
+
+        # endscreen holds frame till click
+        while endscreen:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    endscreen = False
+                    game_running = False
+                    main_loop = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    endscreen = False
     pygame.quit()
 
 
+def get_game():
+    """get the last game id from the csv."""
+    path = 'tic_data.csv'
+    if not os.path.exists(path):
+        return 0
+    df = pd.read_csv(path)
+    return df['game'].iloc[-1]
+
+
 def win_cond(tic_map: []):
+    """check if a player has won."""
+    # rows and columns
     for i in range(3):
         if tic_map[i][0] == tic_map[i][1] == tic_map[i][2] != 0:
             return tic_map[i][0]
         if tic_map[0][i] == tic_map[1][i] == tic_map[2][i] != 0:
             return tic_map[0][i]
 
+    # diagonal
     if tic_map[0][0] == tic_map[1][1] == tic_map[2][2] != 0:
         return tic_map[0][0]
     if tic_map[0][2] == tic_map[1][1] == tic_map[2][0] != 0:
         return tic_map[0][2]
+
+    # no winner yet
     return 0
 
 
 def export(protocol):
+    """append to csv."""
+    header = True
+    path = 'tic_data.csv'
+    if os.path.exists(path):
+        header = False
     df = pd.DataFrame(protocol)
-    df.to_csv('tic_data.csv', mode='a', header=False)
+    df.to_csv(path, mode="a", index=False, header=header)
 
 
 def update_turn(turn: bool):
+    """flip whose turn it is."""
     return not turn
 
 
 def update_tic_map(tic_map: [], move: [], turn: bool):
+    """add last move to the tic map."""
     if turn:
-        tic_map[move[0]][move[1]] = 2
+        tic_map[move[0]][move[1]] = 2  # tic
     else:
-        tic_map[move[0]][move[1]] = 1
+        tic_map[move[0]][move[1]] = 1  # tac
     return tic_map
 
 
 def new_map(size_x: int, size_y: int) -> []:
+    """create new map."""
     grid = []
     for i in range(size_y):
         grid.append([])
@@ -127,6 +184,7 @@ def new_map(size_x: int, size_y: int) -> []:
 
 
 def flip(canvas: [], bit_map: []) -> []:
+    """flip all pixel colors defined in the bitmap."""
     for bit in bit_map:
         if canvas[bit[0]][bit[1]] is True:
             canvas[bit[0]][bit[1]] = False
@@ -136,17 +194,19 @@ def flip(canvas: [], bit_map: []) -> []:
 
 
 def import_art(s):
-    code = []
+    """get bit map from ascii art."""
+    bit_map = []
     rows = s.split('\n')
     rows = rows[1:-1]
     for x, row in enumerate(rows):
         for y, c in enumerate(row):
             if c == '#':
-                code.append([x, y])
-    return code
+                bit_map.append([x, y])
+    return bit_map
 
 
 def get_field(coords: int):
+    """get which field was clicked on."""
     window_l = map_size * cell_size
     field = []
     for c in coords:
@@ -158,6 +218,7 @@ def get_field(coords: int):
 
 
 def place_mark(map: [], coords: int, turn: bool):
+    """place bit_map at the specified field."""
     y = coords[0] * 3
     x = coords[1] * 3
     art = []
